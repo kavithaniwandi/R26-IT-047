9fb534a386ffc132bcacd887e8d84770c4eb0f2d
Kavitha <kavithaekanayaka@gmail.com>
2026-05-12 14:53:55 +0530

    changed folder structure of overall project

--- Frontend/src/pages/History.jsx
+++
@@ -1,9 +0,0 @@
-import RequestHistory from "../components/RequestHistory";
-
-export default function History() {
-  return (
-    <div style={{ minHeight: "100vh", background: "#f0f4f8", paddingTop: "32px" }}>
-      <RequestHistory />
-    </div>
-  );
-}
