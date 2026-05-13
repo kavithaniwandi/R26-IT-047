dff72adecab8901768df74e1256c63a4af804ab8
kavithaniwandi <it22104076@my.sliit.lk>
2026-05-13 11:34:56 +0530

    Delete Backend/python-fastapi directory

--- Backend/python-fastapi/routes/ml_recommendations.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from models.ml_model import predict_demand
-
-router = APIRouter()
-
-@router.post("/predict")
-def predict(data: dict):
-    return predict_demand(data)
